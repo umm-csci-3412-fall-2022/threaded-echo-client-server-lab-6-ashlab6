9716e5e1eb77a0f54c5aaa83deec3a383d048fee
Ash <plase024@morris.umn.edu>
2022-10-13 15:10:56 -0500

    Implement solution in java

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -8,16 +8,76 @@ import java.net.Socket;
 public class EchoClient {
 	public static final int PORT_NUMBER = 6013;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Throwable {
 		EchoClient client = new EchoClient();
 		client.start();
 	}
 
-	private void start() throws IOException {
+	private void start() throws Throwable {
 		Socket socket = new Socket("localhost", PORT_NUMBER);
 		InputStream socketInputStream = socket.getInputStream();
 		OutputStream socketOutputStream = socket.getOutputStream();
 
-		// Put your code here.
+        Thread sending = new Thread(new SendingThread(socketOutputStream));
+        Thread receiving = new Thread(new ReceivingThread(socketInputStream));
+
+		sending.start();
+		receiving.start();
+
+		sending.join();
+		socket.shutdownOutput();
+
+		receiving.join();
+		socket.shutdownInput();
+
+		socket.close();
 	}
+
+	public class SendingThread implements Runnable {
+        OutputStream output;
+
+        SendingThread(OutputStream output) {
+            this.output = output;
+        }
+
+        @Override
+        public void run() {
+            try {
+                int data;
+				while ((data = System.in.read()) != -1) {
+					output.write(data);
+				}
+
+				output.flush();
+            }
+            catch (IOException e) {
+                System.out.println("Sending Thread encountered an error:" + e.toString());
+                System.out.println("Terminating Thread");
+            }
+        }
+    }
+
+	public class ReceivingThread implements Runnable {
+        InputStream input;
+
+        ReceivingThread(InputStream input) {
+            this.input = input;
+        }
+
+        @Override
+        public void run() {
+            try {
+                int data;
+				while ((data = input.read()) != -1) {
+					System.out.write(data);
+				}
+
+				System.out.flush();
+            }
+            catch (IOException e) {
+                System.out.println("Receiving Thread encountered an error:" + e.toString());
+                System.out.println("Terminating Thread");
+            }
+        }
+    }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -7,25 +7,64 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-	
-	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
-	public static final int PORT_NUMBER = 0; 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		EchoServer server = new EchoServer();
-		server.start();
-	}
+    // REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
+    public static final int PORT_NUMBER = 6013; 
+    public static void main(String[] args) throws IOException, InterruptedException {
+        EchoServer server = new EchoServer();
+        server.start();
+    }
 
-	private void start() throws IOException, InterruptedException {
-		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-		while (true) {
-			Socket socket = serverSocket.accept();
+    private void start() throws IOException, InterruptedException {
+        ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+        while (true) {
+            Socket socket = serverSocket.accept();
 
-			// Put your code here.
-			// This should do very little, essentially:
-			//   * Construct an instance of your runnable class
-			//   * Construct a Thread with your runnable
-			//      * Or use a thread pool
-			//   * Start that thread
-		}
-	}
+            ServerThread runner = new ServerThread(socket);
+            new Thread(runner).start();
+        }
+    }
+
+    public class ServerThread implements Runnable {
+        Socket socket;
+
+        ServerThread(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Starting Thread For" + socket.getInetAddress().toString());
+            
+            try {
+                OutputStream out = socket.getOutputStream();
+                InputStream in = socket.getInputStream();
+                
+                // Read data until no more is avaialble
+                while (true) {
+                    int data = in.read();
+
+                    if (data == -1) {
+                        break;
+                    }
+
+                    out.write(data);
+                }
+
+                // Flush to output just to be sure
+                out.flush();
+
+                // Close the output
+                socket.shutdownOutput();
+
+                System.out.println("Stopping Thread For" + socket.getInetAddress().toString());
+            
+                // Close the client socket since we're done.
+                socket.close();
+            }
+            catch (IOException e) {
+                System.out.println("Runner encountered an error:" + e.toString());
+                System.out.println("Terminating Thread");
+            }
+        }
+    }
 }
